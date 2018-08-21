78e48639c5ed04331ed94cd86107c66abfa5e0a8
sushant <haldonkar.sushant@gmail.com>
2018-08-21 23:17:28 +0530

    PDO setup

---
+++ inc/classes/DB.php
@@ -0,0 +1,24 @@
+<?php
+	
+	class DB{
+		protected static $con;
+
+		private function __construct(){
+			try{
+				self::$con = new PDO('mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_course','logincrs_1','logincrs_1');
+				self::$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+				self::$con->setAttribute(PDO::ATTR_PERSISTENT,false);
+			}catch(PDOException $e){
+				echo "Could not connect to database";
+				exit;
+			}
+		}
+
+		public static function getConnection(){
+			if(!self::$con){
+				new DB();
+			}
+			return self::$con;
+		}
+	}
+?>
--- inc/config.php
+++ inc/config.php
@@ -2,4 +2,8 @@
 	if(!defined('__CONFIG__')){
 		exit('You do not have a config file');
 	}
+
+	include_once "classes/DB.php";
+
+	$con = DB::getConnection();
 ?>
